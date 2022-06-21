58a40f88da93c28de0fb3d53bcb7bc6df180d1a7
theLostCode <lostcoding11@ghmail.com>
2022-06-21 23:24:01 +0300

    searchbar updated

--- src/components/users/home/search/searchbar.js
+++ src/components/users/home/search/searchbar.js
@@ -1,39 +1,57 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, InputGroup } from "react-bootstrap";
 import "./searchbar.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const SearchBar = () => {
   return (
-    <div className="search-bar-container">
-      <div className="search-bar">
-        <div className="active">Rent</div>
-        <div className="">Sale</div>
-      </div>
-      <Form className="search-form">
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Type something" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Select id="disabledSelect">
-            <option>Property Category</option>
-            <option>Villa</option>
-            <option>House</option>
-            <option>Land</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Location" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Min Price" />
-          
-          <Form.Control type="text" placeholder="Max Price" />
-        </Form.Group>
-        <Button className="w-100 btn btn-primary btn-lg" variant="primary" type="button">
-          Search
-        </Button>
-      </Form>
-    </div>
+
+    <Container className="container-fluid" >
+      <Row className="search-form g-5">
+        <Col lg={3}>
+        <input type="text" name="name" placeholder="Type something" size={30}/>
+          {/* <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Type something"/>
+          </Form.Group> */}
+        </Col>
+        <Col lg={2}>
+          <Form.Group className="mb-2">
+            <Form.Select id="disabledSelect">
+              <option>Property Category</option>
+              <option>Villa</option>
+              <option>House</option>
+              <option>Land</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col lg={2}>
+          <Form.Group className="mb-2">
+            <Form.Control type="text" placeholder="Location" />
+          </Form.Group>
+        </Col>
+        <Col lg={1}>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Min Price"/>
+          </Form.Group>
+        </Col>
+        <Col lg={1}>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Max Price" />
+          </Form.Group>
+        </Col>
+        <Col lg={1}><BsThreeDotsVertical/>Advanced</Col>
+        <Col lg={2}>
+          <Button
+            className="btn btn-primary btn-lg"
+            variant="primary"
+            type="button"
+          >
+           <AiOutlineSearch/> Search
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
---
+++ src/components/users/home/search/status-bar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import './status-bar.css'
+
+const StatusBar = () => {
+  return (
+    <div className="search-bar">
+        <div className="">Rent</div>
+        <div className="active">Sale</div>
+      </div>
+  )
+}
+
+export default StatusBar
