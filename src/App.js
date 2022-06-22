71cfdc40fe95410107b9aab854861408eee589e0
Gunesahin <mahirihsanazra@gmail.com>
2022-06-22 15:04:42 +0200

    ilk commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import OurPartners from "./components/users/common/OurPartners/OurPartners";
 
 
 function App() {
   return (
     <div className="App">
-yasasin burdayim
+<OurPartners/>
     </div>
   );
 }
