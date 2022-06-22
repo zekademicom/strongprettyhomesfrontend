e4d25d8ca05fa17f3e7338615a762bdedd6b35a0
theLostCode <lostcoding11@ghmail.com>
2022-06-22 22:17:59 +0300

    search updated v3

--- src/components/users/home/search/status-bar.js
+++ src/components/users/home/search/status-bar.js
@@ -1,21 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container } from 'react-bootstrap'
 import './status-bar.css'
 
 const StatusBar = () => {
 
-  let status = "";
-  const active = () => {
-    if(status === "active"){
-     status = ""
-    }else{
-     status = "active"
-    }
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    setIsActive(true);
   }
   return (
     <div className="search-bar">
-        <div className={status} onClick={() => active}>Rent</div>
-        <div className="active">Sale</div>
+        <div className={isActive ? 'active' : ''} onClick={handleClick}>Rent</div>
+        <div className={isActive ? '' : 'active'} onClick={handleClick}>Sale</div>
       </div>
   )
 }
