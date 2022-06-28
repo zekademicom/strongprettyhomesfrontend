2dbe95151b840d657438be0df3af0a34d1484cef
Gunesahin <mahirihsanazra@gmail.com>
2022-06-28 15:53:16 +0200

    s

--- src/pages/users/HomePage.js
+++ src/pages/users/HomePage.js
@@ -4,7 +4,7 @@ import Slider from "../../components/user/home/slider/slider";
 import Testimonals from "../../components/user/home/testimonals/testimonals";
 import WhyUs from "../../components/user/home/whyus/whyus";
 import Spacer from "../../components/user/common/spacer/spacer";
-import OurPartners from "../../components/user/common/ourpartners/OurPartners";
+import OurPartners from "../../components/user/common/ourPartners/OurPartners";
 
 const HomePage = () => {
   return (
@@ -17,7 +17,7 @@ const HomePage = () => {
       <Spacer />
       <Testimonals />
       <Spacer />
-      <OurPartners />
+      <OurPartners/>
       <Spacer />
     </>
   );
