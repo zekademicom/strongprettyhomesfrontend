5ed0f89fcd920b4a9ddce71ee657955e44b64e43
theLostCode <lostcoding11@ghmail.com>
2022-06-29 04:35:36 +0300

    app updated

--- src/store/search/searchActions.js
+++ src/store/search/searchActions.js
@@ -1,6 +1,6 @@
 import { types } from "../types";
 
 export const setSearch = (search) => ({
-    type: types.SET_SEARCH,
-    payload: search
+  type: types.SET_SEARCH,
+  payload: search,
 });
--- src/store/search/searchInitialState.js
+++ src/store/search/searchInitialState.js
@@ -1,4 +1,3 @@
-
 export const searchInitialState = {
-  initialSearchValues: {}
+  initialSearchValues: {},
 };
